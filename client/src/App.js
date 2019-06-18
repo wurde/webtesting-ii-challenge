4961aa8fe76807839cf36493b0b9032b4d2cab64
Andy Bettisworth <andy@accreu.com>
2019-06-18 14:34:48 -0500

    Rework the client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Dependencies
+ */
+
+import React from 'react'
+
+/**
+ * Import global styles
+ */
+
+import './App.scss'
+
+/**
+ * Define component
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
-  );
+  )
 }
 
-export default App;
+/**
+ * Export component
+ */
+
+export default App
