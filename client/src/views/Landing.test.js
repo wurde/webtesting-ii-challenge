f877c60b9e55ac82fb86edb10b6f8548e4d440e5
Andy Bettisworth <andy@accreu.com>
2019-06-18 15:25:27 -0500

    Add Landing.test.js

--- client/src/views/Landing.test.js
+++ client/src/views/Landing.test.js
@@ -4,17 +4,17 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Landing from './Landing'
 
 /**
  * Assertions
  */
 
-describe("App.js", () => {
+describe("Landing.js", () => {
   test('renders without crashing', () => {
     const div = document.createElement('div')
 
-    ReactDOM.render(<App />, div)
+    ReactDOM.render(<Landing />, div)
 
     ReactDOM.unmountComponentAtNode(div)
   })
